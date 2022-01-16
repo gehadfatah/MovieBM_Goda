ed90d82f9f6a9dff08da408ce93aa0aa0881fa28
gehadfatah <gehad.abdelfatah45@gmail.com>
2022-01-17 00:55:33 +0200

    set new style, add svg images, add some extension

---
+++ app/src/main/java/com/goda/movieapp/util/Extensions.kt
@@ -0,0 +1,38 @@
+package com.goda.movieapp.util
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import dagger.android.support.DaggerAppCompatActivity
+
+/*
+fun NavController.navigateSafe(@IdRes resId: Int, args: Bundle? = null) {
+    val destinationId = currentDestination?.getAction(resId)?.destinationId.orEmpty()
+    currentDestination?.let { node ->
+        val currentNode = when (node) {
+            is NavGraph -> node
+            else -> node.parent
+        }
+        if (destinationId != EMPTY_INT) {
+            currentNode?.findNode(destinationId)?.let { navigate(resId, args) }
+        }
+    }}*/
+@SuppressLint("MissingPermission")
+ fun Activity.isNetworkConnected(): Boolean {
+    val connMgr = this.getSystemService(DaggerAppCompatActivity.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkInfo = connMgr.activeNetworkInfo
+    return networkInfo != null && networkInfo.isConnected
+}
+fun Context.showLongToast(msgId: Int) {
+    Toast.makeText(this, getString(msgId), Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showShortToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
